1c0c213dfe006deb5d67abcaa5674f6d96500e17
Salvador Ponce <salvaponce92@gmail.com>
2022-05-19 21:16:43 +0200

    changed settings

--- Price_tracker_app/amazon_traker.py
+++ Price_tracker_app/amazon_traker.py
@@ -41,7 +41,7 @@ class AmazonAPI:
     def set_browser_options(self, options):        
         options.add_argument("--headless")
         options.add_argument('--disable-gpu')
-        #options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--no-sandbox")
 
     def run(self):
