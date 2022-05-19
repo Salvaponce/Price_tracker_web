c7a0e5f4a987356bc7cf9d4d5e72c4d9c02f7532
Salvador Ponce <salvaponce92@gmail.com>
2022-05-19 21:11:43 +0200

    changed settings

--- Price_tracker_app/amazon_traker.py
+++ Price_tracker_app/amazon_traker.py
@@ -42,7 +42,7 @@ class AmazonAPI:
         options.add_argument("--headless")
         options.add_argument('--disable-gpu')
         #options.add_argument("--disable-dev-shm-usage")
-        #options.add_argument("--no-sandbox")
+        options.add_argument("--no-sandbox")
 
     def run(self):
         print(f'Buscando {self.search_term}...')
