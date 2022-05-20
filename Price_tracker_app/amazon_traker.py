c75bf138767ac1247e6953974144f17287901611
Salvador Ponce <salvaponce92@gmail.com>
2022-05-20 12:34:42 +0200

    changed driver options

--- Price_tracker_app/amazon_traker.py
+++ Price_tracker_app/amazon_traker.py
@@ -60,6 +60,10 @@ class AmazonAPI:
     def get_products_links(self):
         self.driver.get(self.base_url)
         self.driver.execute_script("window.open('');")
+        time.sleep(2)
+        self.driver.switch_to.window(self.driver.window_handles[1])
+        self.driver.get("https://duckduckgo.com")
+        time.sleep(2)
         element = self.driver.find_element_by_id("twotabsearchtextbox")
         element.send_keys(self.search_term)
         element.send_keys(Keys.ENTER)
